07136e4a8086bfc0bb2c02eec92c381ce9dd8e5d
yoyo89757001 <332663557@qq.com>
2017-12-22 10:31:27 +0800

    dddddlll

--- app/src/main/java/com/examples/weibao/fargments/Fragment3.java
+++ app/src/main/java/com/examples/weibao/fargments/Fragment3.java
@@ -1,15 +1,22 @@
 package com.examples.weibao.fargments;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,23 +28,14 @@ import com.examples.weibao.allbeans.BaoZhangDengJiBean;
 import com.examples.weibao.allbeans.DengLuBean;
 import com.examples.weibao.allbeans.DengLuBeanDao;
 import com.examples.weibao.dialogs.TiJIaoDialog;
-import com.examples.weibao.ui.BaoZhangDengJiActivity;
-import com.examples.weibao.ui.ChaKanBaoZhangJiLuActivity;
-import com.examples.weibao.ui.HomePageActivity;
-import com.examples.weibao.ui.MainActivity;
 import com.examples.weibao.ui.ZiXunActivity;
-import com.examples.weibao.utils.GsonUtil;
 import com.examples.weibao.utils.Utils;
 import com.github.jdsjlzx.ItemDecoration.DividerDecoration;
 import com.github.jdsjlzx.interfaces.OnItemClickListener;
 import com.github.jdsjlzx.recyclerview.LRecyclerView;
 import com.github.jdsjlzx.recyclerview.LRecyclerViewAdapter;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.sdsmdg.tastytoast.TastyToast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -45,10 +43,12 @@ import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
@@ -56,14 +56,16 @@ import okhttp3.ResponseBody;
  * A simple {@link Fragment} subclass.
  */
 public class Fragment3 extends Fragment {
-    private LRecyclerView lRecyclerView;
-    private LRecyclerViewAdapter lRecyclerViewAdapter;
-    private LinearLayoutManager linearLayoutManager;
-    private BaoZhangJiLuAdapter taiZhangAdapter;
+ //   private LRecyclerView lRecyclerView;
+  //  private LRecyclerViewAdapter lRecyclerViewAdapter;
+   // private LinearLayoutManager linearLayoutManager;
+   // private BaoZhangJiLuAdapter taiZhangAdapter;
     private TiJIaoDialog tiJIaoDialog=null;
     private DengLuBean dengLuBean=null;
     private DengLuBeanDao dengLuBeanDao=null;
-    private List<BaoZhangDengJiBean> plansBeanList=new ArrayList<>();
+  //  private List<BaoZhangDengJiBean> plansBeanList=new ArrayList<>();
+    private WebView webView;
+
 
     public Fragment3() {
         // Required empty public constructor
@@ -77,35 +79,70 @@ public class Fragment3 extends Fragment {
         dengLuBean=dengLuBeanDao.load(123456L);
 
        View view=inflater.inflate(R.layout.fragment_fragment3, container, false);
-        TextView t= (TextView) view.findViewById(R.id.title);
-        t.setText("资讯");
-        ImageView imageView= (ImageView) view.findViewById(R.id.leftim);
-        imageView.setVisibility(View.GONE);
-        lRecyclerView= (LRecyclerView)view.findViewById(R.id.lrecyclerview);
-        taiZhangAdapter=new BaoZhangJiLuAdapter(plansBeanList);
-        lRecyclerViewAdapter = new LRecyclerViewAdapter(taiZhangAdapter);
+       webView= (WebView) view.findViewById(R.id.web);
+//        TextView t= (TextView) view.findViewById(R.id.title);
+//        t.setText("资讯");
+//        ImageView imageView= (ImageView) view.findViewById(R.id.leftim);
+//        imageView.setVisibility(View.GONE);
+//        lRecyclerView= (LRecyclerView)view.findViewById(R.id.lrecyclerview);
+//        taiZhangAdapter=new BaoZhangJiLuAdapter(plansBeanList);
+//        lRecyclerViewAdapter = new LRecyclerViewAdapter(taiZhangAdapter);
+//
+//        linearLayoutManager=new LinearLayoutManager(getActivity());
+//        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+//        lRecyclerView.setLayoutManager(linearLayoutManager);
+//        lRecyclerView.setAdapter(lRecyclerViewAdapter);
+//        lRecyclerView.setPullRefreshEnabled(false);
+//        lRecyclerView.setLoadMoreEnabled(false);
+//
+//        DividerDecoration divider = new DividerDecoration.Builder(getActivity())
+//                .setHeight(R.dimen.default_divider_height)
+//                .setPadding(R.dimen.default_divider_padding)
+//                .setColorResource(R.color.transparent)
+//                .build();
+//        lRecyclerView.addItemDecoration(divider);
+//
+//
+//        lRecyclerViewAdapter.setOnItemClickListener(new OnItemClickListener() {
+//            @Override
+//            public void onItemClick(View view, int position) {
+//
+//                startActivity(new Intent(getActivity(),ZiXunActivity.class));
+//
+//            }
+//        });
+        WebSettings webSettings = webView.getSettings();
+        //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
+        webSettings.setJavaScriptEnabled(true);
+        // 若加载的 html 里有JS 在执行动画等操作，会造成资源浪费（CPU、电量）
+        // 在 onStop 和 onResume 里分别把 setJavaScriptEnabled() 给设置成 false 和 true 即可
+        //支持插件
+        // webSettings.setPluginsEnabled(true);
+        //设置自适应屏幕，两者合用
+        webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
 
-        linearLayoutManager=new LinearLayoutManager(getActivity());
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        lRecyclerView.setLayoutManager(linearLayoutManager);
-        lRecyclerView.setAdapter(lRecyclerViewAdapter);
-        lRecyclerView.setPullRefreshEnabled(false);
-        lRecyclerView.setLoadMoreEnabled(false);
-
-        DividerDecoration divider = new DividerDecoration.Builder(getActivity())
-                .setHeight(R.dimen.default_divider_height)
-                .setPadding(R.dimen.default_divider_padding)
-                .setColorResource(R.color.transparent)
-                .build();
-        lRecyclerView.addItemDecoration(divider);
-
-
-        lRecyclerViewAdapter.setOnItemClickListener(new OnItemClickListener() {
+        //缩放操作
+        webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+        webSettings.setAppCacheEnabled(true);
+        //设置 缓存模式
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        // 开启 DOM storage API 功能
+        webSettings.setDomStorageEnabled(true);
+        //其他细节操作
+      //  webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
+        webSettings.setAllowFileAccess(true); //设置可以访问文件
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
+        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+        webView.setWebViewClient(new WebViewClient() {
+            //覆盖shouldOverrideUrlLoading 方法
             @Override
-            public void onItemClick(View view, int position) {
-
-                startActivity(new Intent(getActivity(),ZiXunActivity.class));
-
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
             }
         });
 
@@ -118,7 +155,7 @@ public class Fragment3 extends Fragment {
     private void link_save() {
         showDialog();
         final MediaType JSON=MediaType.parse("application/json; charset=utf-8");
-        OkHttpClient okHttpClient= MyAppLaction.getOkHttpClient();
+        final OkHttpClient okHttpClient= MyAppLaction.getOkHttpClient();
 
        // String jiami= Utils.jiami(mima).toUpperCase();
         String nonce=Utils.getNonce();
@@ -136,7 +173,7 @@ public class Fragment3 extends Fragment {
 //        }
        // RequestBody body = RequestBody.create(JSON, jsonObject.toString());
 
-        Request.Builder requestBuilder = new Request.Builder()
+        final Request.Builder requestBuilder = new Request.Builder()
                 .header("nonce", nonce)
                 .header("timestamp", timestamp)
                 .header("userId", dengLuBean.getUserId()+"")
@@ -148,7 +185,6 @@ public class Fragment3 extends Fragment {
 
         // step 3：创建 Call 对象
       Call  call = okHttpClient.newCall(requestBuilder.build());
-
         //step 4: 开始异步请求
         call.enqueue(new Callback() {
             @Override
@@ -161,20 +197,42 @@ public class Fragment3 extends Fragment {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 dismissDialog();
-                Log.d("AllConnects", "请求识别成功"+call.request().toString());
+             //   Log.d("AllConnects", "请求识别成功"+call.request().toString());
                 //获得返回体
                 try {
-
-                    ResponseBody body = response.body();
-                    String ss=body.string().trim();
-                    Log.d("InFoActivity", "ss" + ss);
-                    JsonObject jsonObject= GsonUtil.parse(ss).getAsJsonObject();
-                    Gson gson=new Gson();
-                    JsonObject jsonElement= jsonObject.get("account").getAsJsonObject();
-                    DengLuBean zhaoPianBean=gson.fromJson(jsonElement,DengLuBean.class);
-                    if (jsonObject.get("dtoResult").getAsString().equals("0")) {
-                        showMSG(jsonObject.get("dtoDesc").getAsString(), 4);
+                    String strUrl = "http://14.23.169.42:8090/weibao/infoListClient.html";
+                    CookieJar cookieJar = okHttpClient.cookieJar();
+                    HttpUrl url = HttpUrl.parse(strUrl);
+                    List<Cookie> cookies = cookieJar.loadForRequest(url);
+                    StringBuilder sb = new StringBuilder();
+                    for (Cookie cookie : cookies) {
+                        sb.append(cookie.toString());
+                        sb.append(";");
                     }
+                    String strCookies = sb.toString();
+                    android.webkit.CookieManager cookieManager= android.webkit.CookieManager.getInstance();
+                    cookieManager.setCookie(strUrl, strCookies);
+
+                   // synCookies(getActivity(),"http://14.23.169.42:8090/weibao/infoListClient.html");
+                  //  ResponseBody body = response.body();
+                 //   final String ss=body.string().trim();
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            webView.loadUrl("http://14.23.169.42:8090/weibao/infoListClient.html");
+                        }
+                    });
+
+                   // Log.d("InFoActivity", "ss" + ss.substring(1000,ss.length()));
+//                    JsonObject jsonObject= GsonUtil.parse(ss).getAsJsonObject();
+//                    Gson gson=new Gson();
+//                    JsonObject jsonElement= jsonObject.get("account").getAsJsonObject();
+//                    DengLuBean zhaoPianBean=gson.fromJson(jsonElement,DengLuBean.class);
+//                    if (jsonObject.get("dtoResult").getAsString().equals("0")) {
+//                        showMSG(jsonObject.get("dtoDesc").getAsString(), 4);
+//                    }
 
                 }catch (Exception e){
 
@@ -186,6 +244,9 @@ public class Fragment3 extends Fragment {
         });
 
     }
+
+
+
 
     private void showDialog(){
         getActivity().runOnUiThread(new Runnable() {
